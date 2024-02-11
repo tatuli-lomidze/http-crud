e20de1844294990823a38ce1326f35fa2f414adf
tatuli-lomidze <tatulilomidze1@gmail.com>
2024-02-11 15:00:34 -0800

    corrected matched repository

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,22 +1,13 @@
-import UsersContextProvider from "../contexts/UsersContexts";
 import CreatePage from "../pages/CreatePage";
 import DoneTasksPage from "../pages/DoneTasksPage";
 import MainPage from "../pages/MainPage";
 import UpdatePage from "../pages/UpdatePage";
 
 const routes = [
-
-{  element: ( 
-  <div> 
-<UsersContextProvider>
-  <MainPage/>
-  <p>Divider</p>
-  <MainPage/>
-</UsersContextProvider>
-  </div>
-  ),
-  path: "/"},
- 
+  {
+    element: <MainPage />,
+    path: "/",
+  },
 
   { element: <CreatePage />, 
   path: "/create" },
